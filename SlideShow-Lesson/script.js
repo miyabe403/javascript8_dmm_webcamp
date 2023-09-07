ff74e7fad857e160cee9b79265a2aed175d1a156
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-09-08 05:11:32 +0900

    [Add] ページネーション表示の設定

--- SlideShow-Lesson/script.js
+++ SlideShow-Lesson/script.js
@@ -4,7 +4,7 @@ const swiper = new Swiper('.swiper', {
   //オプションの設定
   loop: true, //最後までスライドしたら最初の画像に戻る 
  
-  //ページネーション表示の設定
+  //ページネーション表示の設定 
   pagination: { 
     el: '.swiper-pagination', //ページネーションの要素
   },
